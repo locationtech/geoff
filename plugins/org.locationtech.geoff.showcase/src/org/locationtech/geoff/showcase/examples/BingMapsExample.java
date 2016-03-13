2078f3fe03b745502ba0510e7afb232436f603f4
ekaraca <erdal.karaca.de@googlemail.com>
2016-03-13 13:15:26 +0100

    - fixed copyright headers

--- plugins/org.locationtech.geoff.showcase/src/org/locationtech/geoff/showcase/examples/BingMapsExample.java
+++ plugins/org.locationtech.geoff.showcase/src/org/locationtech/geoff/showcase/examples/BingMapsExample.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2014 Erdal Karaca.
+ * Copyright (c) 2016 Erdal Karaca and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Erdal Karaca - initial API and implementation
- ******************************************************************************/
+ *******************************************************************************/
 package org.locationtech.geoff.showcase.examples;
 
 import static org.locationtech.geoff.core.Geoff.*;
--- plugins/org.locationtech.geoff.ui/src/org/locationtech/geoff/ui/internal/E4PageBookSWTImpl.java
+++ plugins/org.locationtech.geoff.ui/src/org/locationtech/geoff/ui/internal/E4PageBookSWTImpl.java
@@ -178,7 +178,7 @@ public final class E4PageBookSWTImpl extends Composite implements PageBook {
 					if (container.isDisposed()) {
 						return;
 					}
-					
+
 					destroyPage(container, true);
 
 					if (currentPage == container) {
